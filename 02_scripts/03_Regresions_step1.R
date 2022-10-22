8ff373940fb9dde7429510a549c813f1b306d5b7
Guibor Camargo <guibor.camargo@urosario.edu.co>
2022-10-22 01:54:41 -0500

    Tablas de monitoreo

--- 02_scripts/03_Regresions_step1.R
+++ 02_scripts/03_Regresions_step1.R
@@ -3,51 +3,143 @@ library(stargazer)
 library(dplyr)
 library(ggplot2)
 library(did)
+library(kableExtra)
+library(stringr)
 `%!in%` = Negate(`%in%`)
 
-# Cargar matriz inicial
+# 0. Cargar matriz inicial ----
 M0_raw <- readRDS("00_data/M01_hexagonal_matrix_simple.RDS") %>% 
   merge(readRDS("00_data/Matrices_listas_por_componente/C02_Reasentamientos.RDS"),
         by = c("ID_hex", "year"))
 
 
 
-# Variables adicoinmales de legalizaicón
+# 1. Adicion de varaible ----
 M1_incial <- M0_raw %>% 
-  # Variables de tratamiento
-  mutate(T0_treated_group = ifelse(is.na(legalization_year),0,1),
-         
-         T1_treated_period = ifelse(year>= legalization_year ,1,0),
-         T1_treated_period = ifelse(is.na(T1_treated_period),0,
-                                    T1_treated_period),
-          
-         T2_treatment_dynamic = year-legalization_year,
-         T2_treatment_dynamic = ifelse(is.na(T2_treatment_dynamic),0,
-                                       T2_treatment_dynamic),
-         legalization_year = ifelse(is.na(legalization_year),0,legalization_year))
+  mutate(
+    # Tratamientos simples ----
+    ## 1.1. Dummy (time-invariant) si pertenece a grupo de control (legalizacion)
+    T0_treated_group = ifelse(is.na(legalization_year),0,1),
+    
+    ## 1.2. Dummy (time-invariant) si pertenece a grupo de control (MIB)  ----
+    
+    
+    # Arreglos años de tratamientos
+    ## 1.3. Correción de año de legalización ----
+    legalization_year = ifelse(is.na(legalization_year),0,legalization_year),
+    
+    ## 1.4. Corección de año de MIB ----
+    )
 
-table(M1_incial$legalization_year[M1_incial$year == 2005]) # Tratados por año
+
+
+# 2. Tablas de muestras ----
+
+## 2.1. Distribución de observaciones por monitorio ----
+
+mr1 <- M1_incial %>% 
+  mutate(
+    # Grupos experimetnales
+    general_group = case_when(
+      general_group =="tratados despues de 2005"~as.character(legalization_year),
+      general_group == "no tratados"~"Never trated",
+      T~"Treated before 2005"),
+    
+    # Grupos de monitoreo
+    monitoring_group = 
+      case_when(total_monitoring == 15~"I. Always",
+                total_monitoring >0~"II. Not always",
+                T~"III. Never"))
+
+pr <-mr1 %>% group_by(ID_hex) %>% 
+  summarise(across(general_group:monitoring_group,~first(.))) %>% 
+  as.data.frame() %>% 
+  group_by(general_group, monitoring_group) %>% 
+  summarise(n = n()) %>% 
+  as.data.frame() %>% 
+  reshape2::dcast(general_group~monitoring_group, value.var = "n") %>% 
+  rename(`Group` = general_group)  %>% 
+  rowwise() %>% 
+  mutate(Total = sum(c(`I. Always`,`II. Not always`,`III. Never`), na.rm = T))
+  
+
+rbind(
+  pr %>% mutate(across(2:4,~ifelse(is.na(.),"-",as.character(.)))),
+  pr %>% 
+    mutate(across(2:4,~ifelse(is.na(.),"-",
+                              paste0("(",round((100*./Total),1)," %)"))))) %>% 
+  as.data.frame() %>% 
+  arrange(Group) %>% 
+  kbl(format = "latex", booktabs = T, align = "c", 
+    caption = "Distribution observations of tratment status by monitoring group") %>% 
+  add_header_above(c("", "Monitoring frequency group"=3),italic = T, 
+                   align = "c") %>% 
+  pack_rows("Pre-court intervention",1,8) %>%
+  pack_rows("Post-court intervention",9,28, hline_before =  T) %>%
+  pack_rows(" ",29,32, hline_before =  T) %>%
+  collapse_rows(columns = c(1,5), latex_hline = "none")
+    
+  
+
+## 2.2. Distribucuón de Never-treteted por monitoreo
+
+pr <- mr1 %>% filter(neighborhood_order != "treated") %>% 
+  select(ID_hex,monitoring_group, neighborhood_order) %>% 
+  group_by(ID_hex) %>% 
+  summarise(across(1:2,~first(.))) %>% 
+  as.data.frame() %>% 
+  group_by(monitoring_group, neighborhood_order) %>% 
+  summarise(n = n()) %>% 
+  reshape2::dcast(neighborhood_order~monitoring_group, value.var = "n") %>% 
+  rename(`Control group` = neighborhood_order) %>% 
+  rowwise() %>% 
+  mutate(Total = sum(c(`I. Always`,`II. Not always`,`III. Never`), na.rm = T))
+
+
+rbind(
+  pr %>% mutate(across(2:4,~ifelse(is.na(.),"-",as.character(.)))),
+  pr %>% 
+    mutate(across(2:4,~ifelse(is.na(.),"-",
+                              paste0("(",round((100*./Total),1)," %)"))))) %>% 
+  as.data.frame() %>% arrange(`Control group`) %>% 
+  kbl(format = "latex", booktabs = T, align = "c", 
+      caption = "Never trated units' distribution by\n monitoring group") %>% 
+  add_header_above(c("", "Monitoring frequency group"=3),italic = T, 
+                   align = "c") %>% 
+  collapse_rows(columns = c(1,5), latex_hline = "none")
+  
 
 
 ##################
 
-# Filtros para todos
-
-filter(continus_monitoring == T, total_monitoring >= 15, 
-       general_group != "tratados antes de 2005", 
-       year != 2019)
 
 
-# 1. Incondicionales | Controles = Outsiders (pure control)
-M2_prueba <- M1_incial %>% filter(neighborhood_order %in% c("treated","outsider"))
+
+# 1. 
+M2_prueba <- M1_incial %>% 
+  filter(continus_monitoring == T, total_monitoring >= 15, general_group != "tratados antes de 2005")
 
 
-atts <- att_gt(yname = "RES01_identificaion_m2", tname = "year",
+  filter(neighborhood_order %in% c("treated","outsider")) 
+
+
+m1.ili_ocu.nt <- att_gt(yname = "RES01_identificaion_m2", tname = "year",
                idname = "ID_hex", gname = "legalization_year", 
-               data = M2_prueba, # data
+               ## DATA
+               data = M2_prueba %>% filter(neighborhood_order %in% c("treated","outsider")),
+               ###
                xformla = NULL, est_method = "dr", control_group = "nevertreated",
                bstrap = TRUE, biters = 1000, print_details = FALSE, 
                clustervars = "ID_hex", panel = TRUE)
+
+m1.ili_ocu.nt <- att_gt(yname = "RES01_identificaion_m2", tname = "year",
+                        idname = "ID_hex", gname = "legalization_year", 
+                        ## DATA
+                        data = M2_prueba %>% filter(neighborhood_order %in% c("treated","outsider")),
+                        ###
+                        xformla = NULL, est_method = "dr", control_group = "nevertreated",
+                        bstrap = TRUE, biters = 1000, print_details = FALSE, 
+                        clustervars = "ID_hex", panel = TRUE)
 
 
 
